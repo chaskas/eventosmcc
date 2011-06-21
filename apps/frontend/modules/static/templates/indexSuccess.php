5731f405680a0470859705f1c66a8d81f0131907
Rodrigo Campos H <mr.chaskas@gmail.com>
2011-06-21 17:19:39 -0400

    Primer commit

---
+++ apps/frontend/modules/static/templates/indexSuccess.php
@@ -0,0 +1,51 @@
+<!-- Home -->
+<div id="home" class="page">
+  <div class="content-wrapper">
+    <ul id="nav1">
+      <li><a href="#home">Inicio</a></li>
+      <li><a href="#services">Servicios</a></li>
+      <li><a href="#galleria">Galer&iacute;a Fotogr&aacute;fica</a></li>
+      <li><a href="#contact">Cont&aacute;ctanos</a></li>
+    </ul>
+  </div>
+</div>
+<!-- End Home -->
+
+<!-- Servicios -->
+<div id="services" class="page">
+  <div class="content-wrapper">
+    <ul id="nav2">
+      <li><a href="#home">Inicio</a></li>
+      <li><a href="#services">Servicios</a></li>
+      <li><a href="#galleria">Galer&iacute;a Fotogr&aacute;fica</a></li>
+      <li><a href="#contact">Cont&aacute;ctanos</a></li>
+    </ul>
+  </div>
+</div>
+<!-- End Servicios -->
+
+<!-- Galleria -->
+<div id="galleria" class="page">
+  <div class="content-wrapper">
+    <ul id="nav3">
+      <li><a href="#home">Inicio</a></li>
+      <li><a href="#services">Servicios</a></li>
+      <li><a href="#galleria">Galer&iacute;a Fotogr&aacute;fica</a></li>
+      <li><a href="#contact">Cont&aacute;ctanos</a></li>
+    </ul>
+  </div>
+</div>
+<!-- End Galleria -->
+
+<!-- Contact -->
+<div id="contact" class="page">
+  <div class="content-wrapper">
+    <ul id="nav4">
+      <li><a href="#home">Inicio</a></li>
+      <li><a href="#services">Servicios</a></li>
+      <li><a href="#galleria">Galer&iacute;a Fotogr&aacute;fica</a></li>
+      <li><a href="#contact">Cont&aacute;ctanos</a></li>
+    </ul>
+  </div>
+</div>
+<!-- End Contact -->
